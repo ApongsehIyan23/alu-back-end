6612e1822fc4b5f7feff1d772a589faf109f28a0
ApongsehIyan23 <apongsehiyan23@gmail.com>
2026-02-23 21:12:05 +0200

    fixed errors in .py file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,23 +3,16 @@
 import requests
 import sys
 
-
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     base_url = "https://jsonplaceholder.typicode.com"
-
     user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
-    todos = requests.get(
-        "{}/todos".format(base_url), params={"userId": employee_id}
-    ).json()
-
-    employee_name = user.get("name")
+    todos = requests.get("{}/todos?userId={}".format(
+        base_url, employee_id)).json()
+    name = user.get("name")
     total = len(todos)
-    done = [t for t in todos if t.get("completed")]
-    num_done = len(done)
-
+    done = [t for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, num_done, total
-    ))
+        name, len(done), total))
     for task in done:
         print("\t {}".format(task.get("title")))
